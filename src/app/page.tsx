a43120a6e1e776a11b9c32fb6b71301bc8536b89
unknown <mdsabbirrahman2025@gmail.com>
2025-05-30 15:41:36 +0600

    databas added

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
+
+
+export default async function Home() {
+  return <Button className="bg-primary">Hello</Button>
+}
---
+++ src/middleware.ts
@@ -0,0 +1,21 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+
+// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/api/clerk/webhook(.*)'])
+
+export default clerkMiddleware(async (auth, req) => {
+  if (!isPublicRoute(req)) {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+    await auth.protect()
+  }
+})
+
+export const config = {
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
+  ],
+}
